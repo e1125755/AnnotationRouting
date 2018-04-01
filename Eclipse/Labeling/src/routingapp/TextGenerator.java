a3aaeade02aad8379965f052d3ce71921457a951
Jakob Klinger <e1125755@student.tuwien.ac.at>
2018-04-01 19:10:36 +0200

    Paper: No changes. Program: Added TextGenerator.java(unfinished).

---
+++ Eclipse/Labeling/src/routingapp/TextGenerator.java
@@ -0,0 +1,12 @@
+package routingapp;
+/**
+ * String-generating class. Generates correctly formatted texts for use in the routing application.
+ * 
+ * @author Jakob Klinger
+ *
+ */
+public class TextGenerator {
+
+	int wordProb[]={0,};//TODO: Finish this!
+	
+}
